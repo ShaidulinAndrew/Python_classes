bb09be3b6e0250e6e6778f4792268ec8d5f5aa48
Andrew Shaidylin <avshaidylin@gmail.com>
2023-06-19 23:07:18 +1000

    add game in main.py

--- main.py
+++ main.py
@@ -106,3 +106,46 @@ for number in numbers: # проверяем числа в списке
     if numbers.count(number) == 1: # если число встречается в спике один раз, добавляем в переменную
         result.append(number)
 print(result)
+
+# Игра загадать число
+import random
+
+number = random.randint(1, 100)
+print(number)
+
+user_number = None
+count = 0
+levels = {1: 10, 2: 5, 3: 3}
+
+level = int(input('Выберите уровень сложности от 1 до 3: '))
+max_count = levels[level]
+
+user_count = int(input('Введите количество игроков: '))
+users = []
+for i in range(user_count):
+    user_name = input(f'Введите имя игрока {i}: ')
+    users.append(user_name)
+print(users)
+
+is_winner = False
+winner_name = None
+
+while not is_winner:
+    count += 1
+    if count > max_count:
+        print('Все пользователи проиграли')
+        break
+    print(f'Попытка № {count}')
+    for user in users:
+        print(f'Ход пользователя {user}')
+        user_number = int(input('Введите число: '))
+        if user_number == number:
+            is_winner = True
+            winner_name = user
+            break
+        elif number < user_number:
+            print('Ваше число больше загаданного')
+        else:
+            print('Ваше число меньше загаданного')
+else:
+    print(f'Победитель {winner_name}')
